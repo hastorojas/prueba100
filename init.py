4b61ab64ce8d4ce1d764a6dec17c34ce0fd34431
Haroun Asto <hasto22@gmail.com>
2021-06-14 20:44:43 -0500

    hastor: datos en phython

--- init.py
+++ init.py
@@ -1,4 +1,10 @@
-print("Hola")
-print("ciao")
-print("Haroun")
-print("developer")
+intvalor1 = 100
+intvalor2 = 200
+resta = intvalor1 - intvalor2
+
+print('Suma: '+ str(intvalor1 + intvalor2))
+print('Resta: {r}'.format(r=resta))
+print('Multiplicacion: {}'.format(intvalor1 * intvalor2))
+print('Division igual a {2} de {1} y {0}'.format(intvalor1,intvalor2,intvalor2 / intvalor1))
+print(f'Division exacta es: {intvalor2 // intvalor1}')
+print('Residuo: %d'%(intvalor2 % intvalor1)) #desactualizado
