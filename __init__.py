963412bd00a3cd0a794425d100eb932a6e0d50f9
Haroun Asto <hasto22@gmail.com>
2021-06-19 12:37:45 -0500

    hastor: terminado hackaton3

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-from excepcion import ingresa_numero as test
-
-valor = int(input('Ingresa un numero: '))
-
-test(valor)
+n = int(input('Ingrese un numero entero: '))
+    
+if n>=1 and n<=150:
+    for i in range(1,n+1):
+        print(i, end="")
+else:
+    print('Fuera de rango')
