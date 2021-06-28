40a1686eb64497879a832ee2934e85924f6988c1
Haroun Asto <hasto22@gmail.com>
2021-06-27 21:40:00 -0500

    hastor: cambios

--- __init__.py
+++
@@ -1,7 +0,0 @@
-n = int(input('Ingrese un numero entero: '))
-    
-if n>=1 and n<=150:
-    for i in range(1,n+1):
-        print(i, end="")
-else:
-    print('Fuera de rango')
---
+++ objetos/init.py
@@ -0,0 +1,187 @@
+import os
+import time
+import json
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+class Persona():
+     def __init__(self,nombre,dni,telefono):
+        self.nombre = nombre
+        self.dni = dni
+        self.telefono = telefono
+
+
+class Usuario(Persona):    
+        def __init__(self, nombre, dni, telefono,codusuario=0):
+                super().__init__(nombre, dni, telefono)
+                if codusuario == 0:
+                        self.codusuario = self.codusuario + 1
+                else:
+                        self.codusuario = codusuario
+
+        def __str__(self):
+                return str(self.codusuario) + " -> " + str(self.nombre)
+
+        def toDic(self):
+                d = {
+                "nombre": self.nombre,
+                "dni": self.dni,
+                "telefono": self.telefono,
+                "codusuario": self.codusuario
+                }       
+                return d
+
+class Archivo:
+    def __init__(self, nombreArchivo):
+        self.nombreArchivo = nombreArchivo
+
+    def leerArchivo(self):
+        try:
+            file = open(self.nombreArchivo,'r')
+            return file.read()
+        except Exception as e:
+            return e
+        
+
+    def borrarArchivo(self):
+        directorioActual = os.getcwd()
+        path = directorioActual+"\\"+self.nombreArchivo
+        print(path)
+        if(os.path.isfile(path)):
+            try:
+                os.remove(path)
+                print("removiendo archivo")
+
+            except Exception as error:
+                print(error)
+
+    def escribirArchivo(self, linea):
+        try:
+            directorioActual = os.getcwd()
+            path = directorioActual+"\\"+self.nombreArchivo
+            if(os.path.isfile(path)):
+                try:
+                    #escribir el archiv
+                    file = open(self.nombreArchivo, 'a')
+                    file.write(linea + "\n")
+                except Exception as e:
+                    print(e)
+                finally:
+                    file.close()
+            else:
+                file = open(self.nombreArchivo, 'w')
+                file.close()
+                file = open(self.nombreArchivo, 'a')
+                file.write(linea + "\n")
+        except Exception as error:
+            print(error)   
+
+class Menu:
+    def __init__(self, name, op_list, pre_menu=0):
+        self.name = name
+        self.op_list = op_list
+        self.pre_menu = pre_menu
+
+    def show(self):
+        a = True
+        while a:
+            self.limpiarPantalla()
+            print("")
+            print(color.BLUE+"::::::::::::::::::::"+"ESTE ES EL MENU DE " +
+                  self.name.upper()+"::::::::::::::::::::"+color.END)
+            print("")
+            for (key, value) in self.op_list.items():
+                print(key + color.GREEN + " → " + color.END + value)
+            print("")
+            ans = input(
+                color.YELLOW + "Por favor, ingrese su opción: " + color.END)
+            print("")
+            if(ans.upper() == "0"):
+                print("Hasta, pronto")
+                break
+            b = 0
+            for (key, value) in self.op_list.items():
+                if (value == ans):
+                    b = b+1
+            if (b > 0):
+                a = False
+
+            else:
+                print(color.RED + "Opción no valida, escoja una opción valida" + color.END)
+                time.sleep(3)
+        return ans
+
+    def limpiarPantalla(self):
+        def clear():
+            # return os.system('cls')
+            return os.system('clear')
+        clear()
+
+
+listausuarios = []
+listausuarioDic = []
+
+fileCliente = Archivo("contactos.txt")
+
+def cargainicial():
+    res = fileCliente.leerArchivo()
+    print(res)
+    listTempCliente = json.loads(res)
+    for dic in listTempCliente:
+        newCliente = Usuario(dic["nombre"],dic["dni"],dic["telefono"],dic["codusuario"])
+        listausuarios.append(newCliente)
+        listausuarioDic.append(newCliente.toDic())
+
+cargainicial()
+showUsuario = True
+Home_op = {"Agregar Usuario": "1", "Buscar Usuario": "2","Quitar Usuario": "3","Listar Usuarios":"4","Salir": "0"}
+respuesta = ""
+Main_menu = Menu("USUARIOS", Home_op)
+
+while showUsuario:
+        respuesta = Main_menu.show()
+        if(respuesta == "0"):
+                break
+        elif(respuesta == "1"):
+                nombre = input('Ingrese su nombre: ')
+                dni = int(input('Ingrese su DNI: '))
+                telefono = int(input('Ingrese su telefono: '))
+                usuario = Usuario(nombre,dni,telefono,2)
+                listausuarios.append(usuario)
+                listausuarioDic.append(usuario.toDic())
+                jsonString = json.dumps(listausuarioDic)
+                fileCliente.borrarArchivo()
+                fileCliente.escribirArchivo(jsonString)
+                print(usuario)
+                print("Usuario Agregado !!")
+
+        elif (respuesta == "2"):
+            dniusuario = int(input("Ingresa el DNI a buscar: "))
+            for item in listausuarios:
+                if(item.dni == dniusuario):
+                    print("")
+                    print("::::::: Datos del Usuario :::::::")
+                    print("")
+                    print(f"{item.dni} | {item.nombre} | {item.telefono}")             
+                    print("")
+                else:
+                    print("")
+                    print(f"El usuario con DNI: {item.dni} no existe !")
+                    print("")
+            break
+        elif (respuesta == "3"):
+            break
+
+        elif (respuesta == "4"):
+            break
