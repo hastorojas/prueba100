098ee615666ec2bac8b866f8f2196033762e57cd
Haroun Asto <hasto22@gmail.com>
2021-07-08 10:19:26 -0500

    hastor: objetos de bd

--- init.py
+++ init.py
@@ -1,24 +1,7 @@
-from psycopg2 import connect, Error
+from conexion import conexion
 
-try:
 
-    conn = connect(
-    user = "postgres",
-    password = "Nomeacuerdo1701",
-    host = "localhost",
-    database = "postgres",
-    port="5432"
-    )
-
-    cursor = conn.cursor()
-    sql = "select version();"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    print(result)
-
-except(Exception,Error) as error:
-    print(f"Hubo un error {str(error)}")
-finally:
-    if(conn):
-        cursor.close()
-        conn.close()
+conn = conexion()
+query = "select * from profesores"
+result = conn.consultarBDD(query)
+print(result)
