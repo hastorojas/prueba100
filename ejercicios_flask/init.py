b459c29b120a82f12c61221d9698e63124c8cd8a
Haroun Asto <hasto22@gmail.com>
2021-08-06 18:41:15 -0500

    cambios

--- ejercicios_flask/init.py
+++ ejercicios_flask/init.py
@@ -1,5 +1,4 @@
-from flask import Flask, Request
-from flask.globals import request
+from flask import Flask, request, render_template
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -8,24 +7,20 @@ load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
-post = []
+posts = []
 
-@app.route('/')
+@app.route("/")
 def index():
-    render_template("index",num_post=len(post))
+    return render_template("index.html", num_posts=len(posts))
 
-@app.route('/hola',methods=['POST','GET','PUT','PATCH','DELETE'])
-def hola():
-    return f"Hola desde el metodo {request.method}"
-
-@app.route('/p/<string:slug>/')
+@app.route("/p/<string:slug>/")
 def show_post(slug):
-    return render_template("post_view.html",slug_title=slug)
+    return render_template("post_view.html", slug_title=slug)
 
-@app.route('/admin/post/')
-@app.route('/admin/post/<int:post_id>/')
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
 def post_form(post_id=None):
-    return render_template("admin/post_form.html",post_id=post_id)
+    return render_template("admin/post_form.html", post_id=post_id)
 
 if __name__ == "__main__":
     app.run()
