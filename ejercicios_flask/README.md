3c3bad90d752a381ab948fb24cf2d2acac71c946
Haroun Asto <hasto22@gmail.com>
2021-08-06 18:09:39 -0500

    proyecto flask

---
+++ ejercicios_flask/README.md
@@ -0,0 +1,4 @@
+//Ejecutar en la linea de comandos para que el debug se active
+export FLASK_APP="nombredearchivo"
+export FLASK_ENV="development"
+export APP_SETTINGS_MODULE="config.local"
--- ejercicios_flask/init.py
+++ ejercicios_flask/init.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, Request
+from flask.globals import request
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -7,9 +8,24 @@ load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
+post = []
+
 @app.route('/')
-def hello():
-    return "Hello World !!!"
+def index():
+    render_template("index",num_post=len(post))
+
+@app.route('/hola',methods=['POST','GET','PUT','PATCH','DELETE'])
+def hola():
+    return f"Hola desde el metodo {request.method}"
+
+@app.route('/p/<string:slug>/')
+def show_post(slug):
+    return render_template("post_view.html",slug_title=slug)
+
+@app.route('/admin/post/')
+@app.route('/admin/post/<int:post_id>/')
+def post_form(post_id=None):
+    return render_template("admin/post_form.html",post_id=post_id)
 
 if __name__ == "__main__":
     app.run()
