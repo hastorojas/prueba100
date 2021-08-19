2b70b51c51260c33b883b91bb438c49c0e84e870
Haroun Asto <hasto22@gmail.com>
2021-08-18 22:34:43 -0500

    hastor: cambios para el login con bd

--- ejercicios_flask/init.py
+++ ejercicios_flask/init.py
@@ -5,15 +5,19 @@ from forms import SignupForm, PostForm, LoginForm
 from flask_login import LoginManager, logout_user,current_user,login_user,login_required
 from models import users,get_user,User
 from werkzeug.urls import url_parse
+from flask_sqlalchemy import SQLAlchemy
 
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:testing@localhost:5432/miniblog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
+db = SQLAlchemy(app)
 
 posts = []
 
@@ -31,7 +35,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = get_user(form.email.data)
+        user = User.get_by_email(form.email.data)
         if user is not None and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
@@ -59,20 +63,27 @@ def show_signup_form():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = SignupForm()
+    error = None
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
         password = form.password.data
-        # Creamos el usuario y lo guardamos
-        user = User(len(users) + 1, name, email, password)
-        users.append(user)
-        # Dejamos al usuario logueado
-        login_user(user, remember=True)
-        next_page = request.args.get('next', None)
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template("signup_form.html.j2", form=form)
+        # Comprobamos que no hay ya un usuario con ese email
+        user = User.get_by_email(email)
+        if user is not None:
+            error = f'El email {email} ya está siendo utilizado por otro usuario'
+        else:
+            # Creamos el usuario y lo guardamos
+            user = User(name=name, email=email)
+            user.set_password(password)
+            user.save()
+            # Dejamos al usuario logueado
+            login_user(user, remember=True)
+            next_page = request.args.get('next', None)
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template("signup_form.html.j2", form=form, error=error)
 
 @app.route('/logout')
 def logout():
@@ -81,10 +92,7 @@ def logout():
 
 @login_manager.user_loader
 def load_user(user_id):
-    for user in users:
-        if user.id == int(user_id):
-            return user
-    return None
+    return User.get_by_id(int(user_id))
 
 if __name__ == "__main__":
     app.run()
--- ejercicios_flask/models.py
+++ ejercicios_flask/models.py
@@ -1,14 +1,20 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from init import db
 
-class User(UserMixin):
 
-    def __init__(self, id, name, email, password, is_admin=False):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.is_admin = is_admin
+class User(db.Model, UserMixin):
+
+    __tablename__ = 'blog_user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -16,14 +22,15 @@ class User(UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.email)
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
 
-users = []
+    @staticmethod
+    def get_by_id(id):
+        return User.query.get(id)
 
-
-def get_user(email):
-    for user in users:
-        if user.email == email:
-            return user
-    return None
+    @staticmethod
+    def get_by_email(email):
+        return User.query.filter_by(email=email).first()
